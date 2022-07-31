cae46e21a892b4c42fcbbc78c77be04bc30c8ff6
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-07-31 11:21:57 -0600

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # doodle-saver
+
+## Description
+Application allows you to organize thoughts by being able to write and save notes.
+
+
+## Table of Contents (Optional)
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+- [How to Contribute](#contributions)
+- [Tests](#tests)
+
+## Installation
+Clone repo
+
+
+## Usage
+Clone repo, and use node server.js
+
+## Credits
+
+
+## License
+
+MIT License
+
+
+## Features
+saves notes
+
+## How to Contribute
+clone repo, add connection to original owner's repo, make changes and push 
+
+
+## Tests
+
+## Link to walkthrough video
+https://drive.google.com/file/d/1fmKJefWOSlFTIDMI3Ft0bnyKtOqYG2mT/view
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const {
+    v4: uuidv4
+} = require('uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+
+
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+app.get('/api/notes', (req, res) => {
+    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+app.post('/api/notes', (req, res) => {
+
+
+    const {
+        title,
+        text
+    } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
