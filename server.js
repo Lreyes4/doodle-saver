48b0c8170a6a7d8cecc4f045322ac3ecd62cbc9f
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-07-31 16:14:44 -0600

    fixed bug

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
 
 // GET Route for feedback page
 
@@ -48,7 +46,7 @@ const readAndAppend = (content, file) => {
 };
 
 app.get('/api/notes', (req, res) => {
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new UX/UI tip
@@ -73,6 +71,10 @@ app.post('/api/notes', (req, res) => {
         res.error('Error in adding note');
     }
 });
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
